5ac10b094bcd302ea06fa70e6a100e437afb8295
MisQLak <34073547+MisQLak@users.noreply.github.com>
2018-01-15 15:03:27 +0100

    Added dragable ListView with proper layout.

--- app/src/main/java/com/example/android/sleddogquiz/MainActivity.java
+++ app/src/main/java/com/example/android/sleddogquiz/MainActivity.java
@@ -1,27 +1,11 @@
 package com.example.android.sleddogquiz;
 
-import android.content.ClipData;
 import android.content.Intent;
-import android.media.Image;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.DragEvent;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.ListView;
-
-import com.jmedeisis.draglinearlayout.DragLinearLayout;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    public int score = 0;
-    //images for question 1
-    private int im1, im2, im3, im4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +18,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void firstQ(View view) {
-        if (im1 == 0 && im2 == 1 && im3 == 2 && im4 == 3) {
-            score += 1;
-        } else {
-            score = score + 0;
-        }
-        Log.v("test", "działa" + score);
-    }
+
 }
--- app/src/main/java/com/example/android/sleddogquiz/Questionone.java
+++ app/src/main/java/com/example/android/sleddogquiz/Questionone.java
@@ -2,39 +2,37 @@ package com.example.android.sleddogquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import com.mobeta.android.dslv.DragSortListView;
-
-import com.jmedeisis.draglinearlayout.DragLinearLayout;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Questionone extends AppCompatActivity {
+    public int score = 0;
+    //images for question 1
+    private int im1, im2, im3, im4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionone);
         displayListView();
-/*        DragLinearLayout dragLinearLayout = (DragLinearLayout) findViewById(R.id.container);
-        // set all children draggable except the first (the header)
-        for (int i = -1; i < dragLinearLayout.getChildCount(); i++) {
-            View child = dragLinearLayout.getChildAt(i);
-            dragLinearLayout.setViewDraggable(child, child); // the child is its own drag handle
-        }*/
     }
+    ArrayList<DogImage> dogImages;
+    DogImagesAdapter adapter;
     private void displayListView() {
-        final ArrayList<DogImage> dogImages = new ArrayList<DogImage>();
+        dogImages = new ArrayList<DogImage>();
+        adapter = new DogImagesAdapter(this, dogImages);
         //Create a list of images
         dogImages.add(new DogImage(R.drawable.swing));
         dogImages.add(new DogImage(R.drawable.lead));
         dogImages.add(new DogImage(R.drawable.team));
         dogImages.add(new DogImage(R.drawable.wheel));
-        final DogImagesAdapter adapter = new DogImagesAdapter(this, dogImages);
         DragSortListView dragSortListView = (DragSortListView) findViewById(R.id.list);
         dragSortListView.setAdapter(adapter);
+        dragSortListView.setScrollbarFadingEnabled(false);
         dragSortListView.setDropListener(new DragSortListView.DropListener() {
             @Override public void drop(int from, int to) {
                 DogImage movedItem = dogImages.get(from);
@@ -46,4 +44,16 @@ public class Questionone extends AppCompatActivity {
         });
     }
 
+    public void firstQ(View view) {
+        im1 = dogImages.lastIndexOf(R.drawable.wheel);
+        im2 = dogImages.indexOf(R.drawable.team);
+        im3 = dogImages.indexOf(R.drawable.swing);
+        im4 = dogImages.indexOf(R.drawable.lead);
+        if (im1 == 0 && im2 == 1 && im3 == 2 && im4 == 3) {
+            score += 1;
+        } else {
+            score = score + 0;
+        }
+        Log.v("test", "działa" + score + "id to" + im1 + im2 + im3 + im4);
+    }
 }
